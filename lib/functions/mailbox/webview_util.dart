dd918a36e73524b335c88cc12ef52ab6299e6b71
Aly <aly.t.elenany@gmail.com>
2025-08-11 07:55:03 +0300

    fix: adjust font size in app icon widget

--- lib/functions/mailbox/webview_util.dart
+++ lib/functions/mailbox/webview_util.dart
@@ -3,7 +3,6 @@ import 'package:webview_flutter/webview_flutter.dart';
 Future<void> clearWebViewSession() async {
   final cookieManager = WebViewCookieManager();
   await cookieManager.clearCookies();
-  // Clear cache using JavaScript injection
   final controller = WebViewController();
   await controller.runJavaScript("window.localStorage.clear();");
 }
